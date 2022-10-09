4b9fce8836ff525a70ce0090fa33c9cce66e8137
tacrawl <tacrawl@iu.edu>
2022-10-08 21:41:19 -0400

    adding glow effetcs
    Effects

--- Effects/Fever.gd
+++ Effects/Fever.gd
@@ -3,11 +3,25 @@ extends Node2D
 func start_fever():
 	fever()
 	$Timer.start()
+	var fever_inidcator = get_node_or_null("/res/GAme/UI/HUD/Fever")
+	if fever_inidcator != null:
+		fever_inidcator.use_parent_material = false
+	var background = get_node_or_null("/root/Game/Background")
+	if background != null:
+		background.use_parent_material = false
+	
 
 func _on_Timer_timeout():
 	if Global.feverish:
 		fever()
 		$Timer.start()
+	else:
+		var fever_inidcator = get_node_or_null("/res/Game/UI/HUD/Fever")
+		if fever_inidcator != null:
+			fever_inidcator.use_parent_material = true
+		var background = get_node_or_null("/root/Game/Background")
+		if background != null:
+			background.use_parent_material = true
 
 func fever():
 	var ball_container = get_node_or_null("/root/Game/Ball_Container")
